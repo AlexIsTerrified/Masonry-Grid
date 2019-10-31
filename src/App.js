f333e93d8edc08e3cb43f235e81d87e5999b19d9
unknown <alexepicity@gmail.com>
2019-10-31 18:12:02 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+class App extends Component{
+	state = {
+		width: window.innerWidth
+		};
+	
+	handleResize = this.handleResize.bind(this);		
+	
+	handleResize(){
+		this.setState({width:window.innerWidth});
+		}
+	componentDidMount() {
+			window.addEventListener('resize', this.handleResize);
+			fetch("images.json").then(res => res.json()).then(
+						(result) => {
+							this.setState({
+							isLoaded: true,
+							items: result
+							});
+						},
+						(error) => {
+							this.setState({
+								isLoaded: true,
+								error
+							});
+						}
+					)
+			}
+	componentWillUnmount() {
+			window.removeEventListener('resize', this.handleResize);
+		}
+	render() {
+		const { error, isLoaded, items } = this.state;
+    if (error)return (<div>error</div>);
+	else if(!isLoaded)return (<div>loading...</div>);
+	else
+		{
+			if(this.state.width < 460)
+				return (<div className="column">
+						{items.map((ob, a) => (
+							<div key={a}><img src={ob['url']} alt=""></img></div>
+							))}
+					</div>	
+					);
+			else if(this.state.width < 720)
+				return (<React.Fragment>
+					<div className="column">
+						{items.map((ob, a) => {if(a%2===0)return (
+							<div key={a}><img src={ob['url']} alt=""></img></div>
+							)})}
+					</div>
+					<div className="column">
+						{items.map((ob, a) => {if(a%2===1)return (
+							<div key={a}><img src={ob['url']} alt=""></img></div>
+							)})}
+					</div>
+				</React.Fragment>	
+				);
+			else
+				return (<React.Fragment>
+					<div className="column">
+						{items.map((ob, a) => {if(a%3===0)return (
+							<div key={a}><img src={ob['url']} alt=""></img></div>
+							)})}
+					
+					</div>
+					<div className="column">
+						{items.map((ob, a) => {if(a%3===1)return (
+							<div key={a}><img src={ob['url']} alt=""></img></div>
+							)})}
+					</div>
+					<div className="column">
+						{items.map((ob, a) => {if(a%3===2)return (
+							<div key={a}><img src={ob['url']} alt=""></img></div>
+							)})}
+					</div>
+				</React.Fragment>
+					);
+	}
+				}
+		}
 export default App;
